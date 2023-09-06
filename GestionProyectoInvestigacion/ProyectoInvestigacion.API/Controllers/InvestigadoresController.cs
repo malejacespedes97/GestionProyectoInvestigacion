26de7d7feee46413019e804b2010b77c2ca892fb
María Alejandra Correa <malejacespedes@gmail.com>
2023-09-05 22:39:32 -0500

    Listar investigadores

---
+++ GestionProyectoInvestigacion/ProyectoInvestigacion.API/Controllers/InvestigadoresController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoInvestigacion.API.Data;
+
+namespace ProyectoInvestigacion.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]// Habilita el endpoint del controlador
+    public class InvestigadoresController : Controller
+    {
+        private readonly DataContext _context;
+
+        public InvestigadoresController(DataContext context)
+        {
+
+            _context = context;
+        }
+
+        [HttpGet]//get por lista
+        public async Task<ActionResult> Get()
+        {
+            return Ok(await _context.Investigadores.ToListAsync());
+        }
+    }
+}
