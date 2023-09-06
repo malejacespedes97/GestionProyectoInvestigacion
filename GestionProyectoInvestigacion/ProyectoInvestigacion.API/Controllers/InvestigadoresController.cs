8ffcafd9e51a434f850ad8dfc4117426ea0cc93f
María Alejandra Correa <malejacespedes@gmail.com>
2023-09-05 22:54:13 -0500

    Metodo para registrar investigadores

--- GestionProyectoInvestigacion/ProyectoInvestigacion.API/Controllers/InvestigadoresController.cs
+++ GestionProyectoInvestigacion/ProyectoInvestigacion.API/Controllers/InvestigadoresController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoInvestigacion.API.Data;
+using ProyectoInvestigacion.Shared.Entities;
 
 namespace ProyectoInvestigacion.API.Controllers
 {
@@ -20,6 +21,15 @@ namespace ProyectoInvestigacion.API.Controllers
         public async Task<ActionResult> Get()
         {
             return Ok(await _context.Investigadores.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Investigador investigador) //siempre los post son iguales, solo cambia el nombre de la entidad
+        {
+
+            _context.Add(investigador);
+            await _context.SaveChangesAsync();
+            return Ok(investigador);
         }
     }
 }
