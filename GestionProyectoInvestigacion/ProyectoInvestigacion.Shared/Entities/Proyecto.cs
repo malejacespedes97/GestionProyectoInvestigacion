c03fc4d96644f79ffa6bf11d2c080a479636a821
SARA MONCADA ARANGO <saraarango857@gmail.com>
2023-09-06 16:22:45 -0500

    Creación de entidad Proyecto

--- GestionProyectoInvestigacion/ProyectoInvestigacion.Shared/Entities/Proyecto.cs
+++ GestionProyectoInvestigacion/ProyectoInvestigacion.Shared/Entities/Proyecto.cs
@@ -16,7 +16,30 @@ namespace ProyectoInvestigacion.Shared.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Name { get; set; } = null;
 
+        [Display(Name = "Fecha de inicio")]
+        [MaxLength(10)]
+        [Required(ErrorMessage = "El campo fecha de inicio es obligatorio")]
+        public string FechaInicio { get; set; } = null;
 
+        [Display(Name = "Fecha de finalización")]
+        [MaxLength(10)]
+        [Required(ErrorMessage = "El campo fecha de finalización es obligatorio")]
+        public string FechaFin { get; set; } = null;
+
+        [Display(Name = "Líder de Proyecto")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo líder es obligatorio")]
+        public string Lider { get; set; } = null;
+
+        [Display(Name = "Descripción de Proyecto")]
+        [MaxLength(200)]
+        [Required(ErrorMessage = "El campo descripción es obligatorio")]
+        public string Descripcion { get; set; } = null;
+
+        [Display(Name = "Área de investigación")]
+        [MaxLength(100)]
+        [Required(ErrorMessage = "El campo área es obligatorio")]
+        public string AreaInvest { get; set; } = null;
 
     }
 }
