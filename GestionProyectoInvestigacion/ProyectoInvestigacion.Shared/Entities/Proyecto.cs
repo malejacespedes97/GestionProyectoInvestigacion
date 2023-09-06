eb23832be5ffdba24aa892e0b7440ade7caae88f
SARA MONCADA ARANGO <saraarango857@gmail.com>
2023-09-06 16:09:06 -0500

    Se crea Entity Proyecto

---
+++ GestionProyectoInvestigacion/ProyectoInvestigacion.Shared/Entities/Proyecto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoInvestigacion.Shared.Entities
+{
+    public class Proyecto
+    {
+        public int Id { get; set; }
+
+        [Display (Name = "Nombre")]
+        [MaxLength(100)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Name { get; set; } = null;
+
+
+
+    }
+}
